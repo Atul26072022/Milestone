dae467c3ea0e1475192f86cd42c3b43e9ab68a20
Atul26072022 <110164875+Atul26072022@users.noreply.github.com>
2022-08-12 11:20:35 +0530

    Delete UserAuth directory

--- UserAuth/authuser/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import userdata
-# Register your models here.
-
-@admin.register(userdata)
-class userdataAdmin(admin.ModelAdmin):
-    list_display = ['id','name','email','password','token','verify']
